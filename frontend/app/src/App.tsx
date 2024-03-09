6d0a6a31a73b17739a99bd3148409270be58e054
kai-0307 <93191804+kai-0307@users.noreply.github.com>
2024-03-10 00:55:31 +0900

    サイドバー作成

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,18 +1,20 @@
-import './App.css'
-import {RouterConfig} from '../router/route'
-import { Header } from './components/Header/Header'
-import { BrowserRouter } from 'react-router-dom'
+import "./App.css";
+import { RouterConfig } from "../router/route";
+import { Header } from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
-    {/* react-router-domをコンポーネント内で使用するにはBrowserRouterで囲む必要がある */}
+      {/* react-router-domをコンポーネント内で使用するにはBrowserRouterで囲む必要がある */}
       <BrowserRouter>
         <Header />
       </BrowserRouter>
       <RouterConfig />
+      <Sidebar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
